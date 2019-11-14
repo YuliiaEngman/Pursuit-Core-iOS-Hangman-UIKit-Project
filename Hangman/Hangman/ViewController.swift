58a99c96f72e3fb7d39686e4ea1440ffc4521853
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-14 15:50:09 -0500

    VC with set labels

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var player1UserInputTF: UITextField!
+    @IBOutlet weak var hangingImage: UIImageView!
+    @IBOutlet weak var displayedWordLabel: UILabel!
+    @IBOutlet weak var player2EnterLetterTF: UITextField!
+    @IBOutlet weak var newGameButton: UIButton!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+    
+    
+    @IBAction func newGamePressed(_ sender: UIButton) {
+    }
+    
 
 
 }
